9e0f15e039c18eef5af72adccf433fbdb82a0778
Krisztian Fodor <kris.fodor66@gmail.com>
2023-08-04 15:08:46 +0200

    integration-test-controllers-final

--- SkyTracker.Controllers.Tests/AccountManagementControllerTests.cs
+++ SkyTracker.Controllers.Tests/AccountManagementControllerTests.cs
@@ -1,46 +1,42 @@
-﻿using System.Security.Claims;
+﻿namespace SkyTracker.Controllers.Tests;
+
+using System.Security.Claims;
+
+using Data.Models;
 
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 
 using Moq;
 
-using SkyTracker.Web.ViewModels.AccountManagement;
-
-namespace SkyTracker.Controllers.Tests;
-
-using Azure.Storage.Blobs;
-
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.Extensions.DependencyInjection;
-
-using Data.Models;
 using SkyTracker.Web.Controllers;
+
+using Web.ViewModels.AccountManagement;
 
 public class AccountManagementControllerTests
 {
     private AccountManagementController _accountManagementController;
     private Mock<UserManager<ApplicationUser>> _mockUserManager;
+    private Mock<SignInManager<ApplicationUser>> _mockSignInManager;
     private PasswordChangeModel _validPasswordChangeModel;
-    private EmailChangeModel _validEmailChangeModel;
 
     [SetUp]
     public void Setup()
     {
-        _mockUserManager = new Mock<UserManager<ApplicationUser>>(Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
+        _mockUserManager = new Mock<UserManager<ApplicationUser>>(
+            Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
 
-        var mockHttpContext = new DefaultHttpContext();
-        mockHttpContext.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
-        {
-            new Claim(ClaimTypes.NameIdentifier, "testUserId")
-        }));
+        _mockSignInManager = new Mock<SignInManager<ApplicationUser>>(
+            _mockUserManager.Object, Mock.Of<IHttpContextAccessor>(), Mock.Of<IUserClaimsPrincipalFactory<ApplicationUser>>(), null, null, null, null);
 
-        _accountManagementController = new AccountManagementController(_mockUserManager.Object, null, null, null)
+        _accountManagementController = new AccountManagementController(
+            _mockUserManager.Object, _mockSignInManager.Object, null, null)
         {
             ControllerContext = new ControllerContext()
             {
-                HttpContext = mockHttpContext
+                HttpContext = GetMockHttpContext()
             }
         };
 
@@ -50,11 +46,17 @@ public class AccountManagementControllerTests
             NewPassword = "newPassword",
             ConfirmPassword = "newPassword"
         };
+    }
 
-        _validEmailChangeModel = new EmailChangeModel
+    private static HttpContext GetMockHttpContext()
+    {
+        var context = new DefaultHttpContext();
+        context.User = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
         {
-            NewEmail = "newemail@example.com"
-        };
+            new Claim(ClaimTypes.NameIdentifier, "f47ac10b-58cc-4372-a567-0e02b2c3d479")
+        }));
+
+        return context;
     }
 
     [Test]
@@ -98,28 +100,351 @@ public class AccountManagementControllerTests
         Assert.AreEqual($"Unable to load user with ID ''.", notFoundResult.Value);
     }
 
-    //
-    // Picture Upload not tested because it is handled by Azure Blob Storage
-    //
+    /*
+        Picture Upload not tested because it is handled by Azure Blob Storage
+    */
 
     [Test]
     public async Task PasswordChange_Get_ReturnsViewWithModel()
     {
-        // Arrange
-
-        // Act
         var result = await _accountManagementController.PasswordChange();
 
-        // Assert
         Assert.IsInstanceOf<ViewResult>(result);
         var viewResult = result as ViewResult;
         Assert.IsInstanceOf<PasswordChangeModel>(viewResult.Model);
     }
 
+    [Test]
+    public async Task PasswordChange_ValidModel_ReturnsRedirectToAction()
+    {
+        var mockHttpContext = GetMockHttpContext();
+        _accountManagementController.ControllerContext = new ControllerContext()
+        {
+            HttpContext = mockHttpContext
+        };
+
+        string userId = mockHttpContext.User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+        var defaultUser = new ApplicationUser { Id = Guid.Parse(userId) };
+
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>()))
+            .ReturnsAsync(defaultUser);
+
+        _mockUserManager.Setup(u => u.ChangePasswordAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>(), It.IsAny<string>()))
+            .ReturnsAsync(IdentityResult.Success);
+
+        var tempData = new TempDataDictionary(_accountManagementController.ControllerContext.HttpContext, Mock.Of<ITempDataProvider>());
+        _accountManagementController.TempData = tempData;
+
+        var result = await _accountManagementController.PasswordChange(_validPasswordChangeModel);
+
+        Assert.IsInstanceOf<RedirectToActionResult>(result);
+
+        var redirectResult = (RedirectToActionResult)result;
+
+        Assert.AreEqual("Index", redirectResult.ActionName);
+        Assert.AreEqual("Home", redirectResult.ControllerName);
+
+        Assert.AreEqual("Your password has been changed.", _accountManagementController.StatusMessage);
+    }
+
+    [Test]
+    public async Task PasswordChange_ShouldThrow_ReturnsNotFoundUserNull()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>()))
+            .ReturnsAsync((ApplicationUser)null);
+
+        var result = await _accountManagementController.PasswordChange(_validPasswordChangeModel);
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+        var notFoundResult = (NotFoundObjectResult)result;
+
+        Assert.AreEqual($"Unable to load user with ID ''.", notFoundResult.Value);
+    }
+
+    [Test]
+    public async Task EmailChange_Get_UserFound_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+
+        var result = await _accountManagementController.EmailChange();
+
+        Assert.IsInstanceOf<ViewResult>(result);
+
+        var viewResult = (ViewResult)result;
+        Assert.IsInstanceOf<EmailChangeModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task EmailChange_Get_UserNotFound_ReturnsNotFound()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _accountManagementController.EmailChange();
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+    }
+
+    [Test]
+    public async Task EmailChange_Post_UserFound_EmailChangeSuccess_ReturnsRedirectToAction()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.SetEmailAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+
+        var tempData = new TempDataDictionary(_accountManagementController.ControllerContext.HttpContext, Mock.Of<ITempDataProvider>());
+        _accountManagementController.TempData = tempData;
+
+        var result = await _accountManagementController.EmailChange(new EmailChangeModel());
+
+        Assert.IsInstanceOf<RedirectToActionResult>(result);
+        var redirectResult = (RedirectToActionResult)result;
+
+        Assert.AreEqual("Index", redirectResult.ActionName);
+        Assert.AreEqual("Home", redirectResult.ControllerName);
+
+        Assert.AreEqual("Your email has been changed.", _accountManagementController.StatusMessage);
+    }
+
+    [Test]
+    public async Task EmailChange_Post_UserFound_EmailChangeFailed_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.SetEmailAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed(new IdentityError { Description = "Email change failed." }));
+
+        var result = await _accountManagementController.EmailChange(new EmailChangeModel());
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+        Assert.IsInstanceOf<EmailChangeModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task EmailChange_Post_UserNotFound_ReturnsNotFound()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _accountManagementController.EmailChange(new EmailChangeModel());
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+
+        var notFoundResult = (NotFoundObjectResult)result;
+
+        Assert.AreEqual($"Unable to load user with ID ''.", notFoundResult.Value);
+    }
+
+    [Test]
+    public async Task EmailChange_Post_InvalidModelState_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+
+        _accountManagementController.ModelState.AddModelError("NewEmail", "The NewEmail field is required.");
+
+        var result = await _accountManagementController.EmailChange(new EmailChangeModel());
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+        Assert.IsInstanceOf<EmailChangeModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task PhoneNumberChange_Get_UserFound_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+
+        var result = await _accountManagementController.PhoneNumberChange();
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+        Assert.IsInstanceOf<PhoneNumberChangeModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task PhoneNumberChange_Get_UserNotFound_ReturnsNotFound()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _accountManagementController.PhoneNumberChange();
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+
+        var notFoundResult = (NotFoundObjectResult)result;
+
+        Assert.AreEqual($"Unable to load user with ID ''.", notFoundResult.Value);
+    }
+
+    [Test]
+    public async Task PhoneNumberChange_Post_UserFound_PhoneNumberChangeSuccess_ReturnsRedirectToAction()
+    {
+        var user = new ApplicationUser();
+
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.GetPhoneNumberAsync(It.IsAny<ApplicationUser>())).ReturnsAsync("1234567890");
+
+        _mockUserManager.Setup(u => u.SetPhoneNumberAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Success);
+
+        var tempData = new TempDataDictionary(_accountManagementController.ControllerContext.HttpContext, Mock.Of<ITempDataProvider>());
+        _accountManagementController.TempData = tempData;
+
+        var result = await _accountManagementController.PhoneNumberChange(new PhoneNumberChangeModel());
 
 
+        Assert.IsInstanceOf<RedirectToActionResult>(result);
+        var redirectResult = (RedirectToActionResult)result;
+        Assert.AreEqual("Index", redirectResult.ActionName);
+        Assert.AreEqual("Home", redirectResult.ControllerName);
+
+        Assert.AreEqual("Your phone number has been changed.", _accountManagementController.StatusMessage);
+    }
+
+    [Test]
+    public async Task PhoneNumberChange_Post_UserFound_PhoneNumberChangeFailed_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.GetPhoneNumberAsync(It.IsAny<ApplicationUser>())).ReturnsAsync("1234567890");
+
+        _mockUserManager.Setup(u => u.SetPhoneNumberAsync(It.IsAny<ApplicationUser>(), It.IsAny<string>())).ReturnsAsync(IdentityResult.Failed(new IdentityError { Description = "Phone number change failed." }));
+
+        var result = await _accountManagementController.PhoneNumberChange(new PhoneNumberChangeModel());
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+        Assert.IsInstanceOf<PhoneNumberChangeModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task PhoneNumberChange_Post_UserFound_InvalidModelState_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+
+        _accountManagementController.ModelState.AddModelError("NewPhoneNumber", "The NewPhoneNumber field is required.");
+
+        var result = await _accountManagementController.PhoneNumberChange(new PhoneNumberChangeModel());
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+        Assert.IsInstanceOf<PhoneNumberChangeModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task PhoneNumberChange_Post_UserNotFound_ReturnsNotFound()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _accountManagementController.PhoneNumberChange(new PhoneNumberChangeModel());
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+        var notFoundResult = (NotFoundObjectResult)result;
+
+        Assert.AreEqual($"Unable to load user with ID ''.", notFoundResult.Value);
+    }
+
+    [Test]
+    public async Task DownloadPersonalData_UserFound_ReturnsJsonFile()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.GetLoginsAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(new List<UserLoginInfo>());
+
+        _mockUserManager.Setup(u => u.GetAuthenticatorKeyAsync(It.IsAny<ApplicationUser>())).ReturnsAsync("AuthKey");
+
+        var result = await _accountManagementController.DownloadPersonalData();
+
+        Assert.IsInstanceOf<FileContentResult>(result);
+        var fileResult = (FileContentResult)result;
+        Assert.AreEqual("PersonalData.json", fileResult.FileDownloadName);
+        Assert.AreEqual("application/json; charset=utf-16", fileResult.ContentType);
+    }
+
+    [Test]
+    public async Task DownloadPersonalData_UserFound_NoPersonalData_ReturnsJsonFile()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.GetLoginsAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(new List<UserLoginInfo>());
+
+        _mockUserManager.Setup(u => u.GetAuthenticatorKeyAsync(It.IsAny<ApplicationUser>())).ReturnsAsync("AuthKey");
+
+        var result = await _accountManagementController.DownloadPersonalData();
 
 
+        Assert.IsInstanceOf<FileContentResult>(result);
+        var fileResult = (FileContentResult)result;
+        Assert.AreEqual("PersonalData.json", fileResult.FileDownloadName);
+        Assert.AreEqual("application/json; charset=utf-16", fileResult.ContentType);
+    }
 
+    [Test]
+    public async Task DownloadPersonalData_UserNotFound_ReturnsNotFound()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync((ApplicationUser)null);
 
+        var result = await _accountManagementController.DownloadPersonalData();
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+    }
+
+    [Test]
+    public async Task DeletePersonalData_Post_UserFound_ConfirmationMatches_ReturnsRedirectToAction()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+        _mockUserManager.Setup(u => u.UpdateAsync(It.IsAny<ApplicationUser>())).ReturnsAsync(IdentityResult.Success);
+
+        var result = await _accountManagementController.DeletePersonalData(new DeletePersonalDataModel { Confirmation = "DELETE" });
+
+        Assert.IsInstanceOf<RedirectToActionResult>(result);
+        var redirectResult = (RedirectToActionResult)result;
+
+        Assert.AreEqual("Index", redirectResult.ActionName);
+        Assert.AreEqual("Home", redirectResult.ControllerName);
+    }
+
+    [Test]
+    public async Task DeletePersonalData_Post_UserFound_ConfirmationDoesNotMatch_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+
+        var result = await _accountManagementController.DeletePersonalData(new DeletePersonalDataModel { Confirmation = "WRONG" });
+
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+
+        Assert.IsInstanceOf<DeletePersonalDataModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task DeletePersonalData_Post_UserFound_InvalidModelState_ReturnsViewWithModel()
+    {
+        var user = new ApplicationUser();
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync(user);
+
+        var result = await _accountManagementController.DeletePersonalData(new DeletePersonalDataModel());
+        
+        Assert.IsInstanceOf<ViewResult>(result);
+        var viewResult = (ViewResult)result;
+
+        Assert.IsInstanceOf<DeletePersonalDataModel>(viewResult.Model);
+    }
+
+    [Test]
+    public async Task DeletePersonalData_Post_UserNotFound_ReturnsNotFound()
+    {
+        _mockUserManager.Setup(u => u.GetUserAsync(It.IsAny<ClaimsPrincipal>())).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _accountManagementController.DeletePersonalData(new DeletePersonalDataModel { Confirmation = "DELETE" });
+
+        Assert.IsInstanceOf<NotFoundObjectResult>(result);
+        var notFoundResult = (NotFoundObjectResult)result;
+
+        Assert.AreEqual($"Unable to load user with ID ''.", notFoundResult.Value);
+    }
 }
---
+++ SkyTracker.Controllers.Tests/UserManagementControllerTests.cs
@@ -0,0 +1,226 @@
+﻿namespace SkyTracker.Controllers.Tests;
+
+using System.Text.Json;
+
+using Data.Models;
+
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+using Moq;
+
+using SkyTracker.Services.Data.Interfaces;
+using SkyTracker.Web.Controllers;
+
+public class UserManagementControllerTests
+{
+    private UserManagementController _userManagementController;
+    private Mock<IUserManagementService> _mockUserManagementService;
+    private Mock<UserManager<ApplicationUser>> _mockUserManager;
+
+    [SetUp]
+    public void Setup()
+    {
+        _mockUserManagementService = new Mock<IUserManagementService>();
+        _mockUserManager = new Mock<UserManager<ApplicationUser>>(Mock.Of<IUserStore<ApplicationUser>>(), null, null, null, null, null, null, null, null);
+
+        _userManagementController = new UserManagementController(_mockUserManagementService.Object, _mockUserManager.Object);
+    }
+
+    private List<ApplicationUser> GetSampleUsers()
+    {
+        var users = new List<ApplicationUser>
+        {
+            new ApplicationUser { Id = Guid.NewGuid(), UserName = "user1" },
+            new ApplicationUser { Id = Guid.NewGuid(), UserName = "user2"}
+        };
+
+        return users;
+    }
+
+    private static string ToJsonString(object obj)
+    {
+        return JsonSerializer.Serialize(obj, new JsonSerializerOptions
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = true
+        });
+    }
+
+    /*
+        Tests for UsersPartial, ModeratorListPartial and LockedUsersPartial are omitted because their main functionality was tested in the UserManagementServiceTests.
+        The untested part is the Pagination, but that uses a third-party library called X.PagedList.
+    */
+
+    [Test]
+    public async Task Promote_ValidUserIds_SuccessfulPromotion()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user1")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("user2")).ReturnsAsync(users.Find(u => u.UserName == "user2"));
+
+        var result = await _userManagementController.Promote(new[] { "user1", "user2" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    [Test]
+    public async Task Promote_UserNotFound_ReturnsSuccessForOtherUsers()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("notfound")).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _userManagementController.Promote(new[] { "user", "notfound" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    /*
+        Non Existing User Ids and Not Selecting Ids logic is handled on the client side with JavaScript. This applies to Promote, Demote, Lockout and Unlock methods.
+    */
+
+    [Test]
+    public async Task Demote_ValidUserIds_SuccessfulDemotion()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user1")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("user2")).ReturnsAsync(users.Find(u => u.UserName == "user2"));
+
+        var result = await _userManagementController.Demote(new[] { "user1", "user2" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    [Test]
+    public async Task Demote_UserNotFound_ReturnsSuccessForOtherUsers()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("notfound")).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _userManagementController.Demote(new[] { "user", "notfound" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    [Test]
+    public async Task Lockout_ValidUserIds_SuccessfulLockOut()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user1")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("user2")).ReturnsAsync(users.Find(u => u.UserName == "user2"));
+
+        var result = await _userManagementController.Lockout(new[] { "user1", "user2" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    [Test]
+    public async Task Lockout_UserNotFound_ReturnsSuccessForOtherUsers()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("notfound")).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _userManagementController.Lockout(new[] { "user", "notfound" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    [Test]
+    public async Task Unlock_ValidUserIds_SuccessfulUnlock()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user1")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("user2")).ReturnsAsync(users.Find(u => u.UserName == "user2"));
+
+        var result = await _userManagementController.Unlock(new[] { "user1", "user2" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+
+    [Test]
+    public async Task Unlock_UserNotFound_ReturnsSuccessForOtherUsers()
+    {
+        var users = GetSampleUsers();
+
+        _mockUserManager.Setup(u => u.FindByIdAsync("user")).ReturnsAsync(users.Find(u => u.UserName == "user1"));
+        _mockUserManager.Setup(u => u.FindByIdAsync("notfound")).ReturnsAsync((ApplicationUser)null);
+
+        var result = await _userManagementController.Unlock(new[] { "user", "notfound" });
+
+        Assert.IsInstanceOf<JsonResult>(result);
+        var jsonResult = (JsonResult)result;
+        dynamic data = jsonResult.Value;
+
+        var expectedJson = ToJsonString(new { success = true });
+
+        var actualJson = ToJsonString(data);
+
+        Assert.AreEqual(expectedJson, actualJson);
+    }
+}
